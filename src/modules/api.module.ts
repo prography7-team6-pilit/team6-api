b3cd57ea47ce4457b3199fbff15ed3e2d9601a5d
Lee Jae Jun <linho301150@gmail.com>
2022-05-01 14:09:09 +0900

    Remove : File api.service.ts , Scheduler module #1

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -1,14 +1,10 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
-import { ApiService } from './api.service';
 import { MessageQueueModule } from './message_queue/message_queue.module';
 import { SampleModule } from './sample';
 
 @Module({
-	imports: [SampleModule,MessageQueueModule,
-		ScheduleModule.forRoot()],
-	providers: [ApiService],
+	imports: [SampleModule,MessageQueueModule],
 
 })
 export class ApiModule {}
