2c2bed52bd3fb3a8bf2f9438aa3ed2c70973b548
Lee Jae Jun <linho301150@gmail.com>
2022-05-04 17:34:30 +0900

    Init : TypeOrm, Message_queue, Pill_manage

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -1,7 +1,13 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FirebaseCloudMessagingModule } from './firebase_cloud_messaging';
 import { MessageQueueModule } from './message_queue';
+import { PillManageModule } from './pill_manage/pill_manage.module';
+import { Job } from './repo/entity/job.entity';
+import { Pill } from './repo/entity/pill.entity';
+import { User } from './repo/entity/user.entity';
+import { RepositoryModule } from './repo/repo.module';
 import { SampleModule } from './sample';
 
 @Module({
@@ -12,7 +18,21 @@ import { SampleModule } from './sample';
 				port: 6379,
 			  },
 		  }),
-		  SampleModule,MessageQueueModule,FirebaseCloudMessagingModule,
+		  TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: '127.0.0.1',
+			port: 3306,
+			username: 'root',
+			password: 'root',
+			database: 'pillit',
+			entities: [Job,User,Pill],
+			synchronize: true,
+		  }),
+		  SampleModule,
+		  MessageQueueModule,
+		  FirebaseCloudMessagingModule,
+		  RepositoryModule,
+		  PillManageModule
 		],
 })
 export class ApiModule {}
