bb6315cab28870d4defc284d8c787c7053904954
Lee Jae Jun <linho301150@gmail.com>
2022-05-08 04:50:34 +0900

    Feat : 사용자 접근 , 사용자 닉네임 설정 기능 추가

--- src/modules/user_manage/user_manage.module.ts
+++ src/modules/user_manage/user_manage.module.ts
@@ -1,10 +1,24 @@
+import { RepositoryModule } from '@modules/repo';
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './dto/jwt.strategy';
 import { UserManageController } from './user_manage.controller';
 import { UserManageService } from './user_manage.service';
 
 @Module({
+  imports:[
+    PassportModule.register({defaultStrategy:'jwt'}),
+    JwtModule.register({
+      secret:"asdf",
+      signOptions:{
+        expiresIn:60*60,
+      }
+    }),
+    RepositoryModule
+  ],
   controllers: [UserManageController],
-  providers: [UserManageService],
-  exports: [UserManageService],
+  providers: [UserManageService,JwtStrategy],
+  exports: [UserManageService,PassportModule],
 })
 export class UserManageModule {}
--- src/modules/user_manage/user_manage.service.ts
+++ src/modules/user_manage/user_manage.service.ts
@@ -1,8 +1,33 @@
+import { RepositoryService } from '@modules/repo/repo.service';
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserRequestDto } from './dto/user.request.dto';
 
 @Injectable()
 export class UserManageService {
-    getSample(): string {
-		return 'sample';
+	constructor(private readonly jwtService:JwtService,
+		private readonly repo:RepositoryService){}
+		
+	async signIn(uuid:string):Promise<string | undefined>{
+		try{
+			const result=await this.repo.getNickname(uuid);
+			const payload={...result};
+			const accessToken = this.jwtService.sign(payload);
+			return accessToken;
+		}
+		catch{
+			return undefined
+		}
+	}
+	async signUp(userDto:UserRequestDto):Promise<string | undefined>{
+		try{
+			const result = await this.repo.setNickname(userDto);
+			const payload={result};
+			const accessToken = await this.jwtService.sign(payload);
+			return accessToken;	
+		}
+		catch{
+			return undefined
+		}
 	}
 }
