f067b45075a9a92a488ad60984674fbd502e622f
Lee Jae Jun <linho301150@gmail.com>
2022-05-08 04:52:37 +0900

    Feat : 약 검색, 약 복용여부 기능 추가
    월별 복용내용 조회 기능 수정중

--- src/modules/pill_manage/pill_manage.controller.ts
+++ src/modules/pill_manage/pill_manage.controller.ts
@@ -1,28 +1,53 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res, UseGuards, ValidationPipe } from '@nestjs/common';
+import { ApiBearerAuth, ApiCreatedResponse, ApiForbiddenResponse, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { PillResponseErrorDto } from './dto/pill.error.dto';
+import { EatRequestDto } from './dto/eat.request.post.dto';
+import { PillResponseDto } from './dto/pill.response.dto';
+import { PillManageService } from './pill_manage.service';
+import { EatResponseDto } from './dto/eat.response.post.dto';
+import { JwtAuthGuard } from '@modules/user_manage/user_manage.guard';
 
 @Controller({
 	version: '1',
-	path: 'pill-manage',
+	path: 'pill',
 })
+@ApiTags('약 관련 API')
 export class PillManageController {
-	@Get('/')
-	getAllPill() {
-		return 'pill';
+    constructor(private readonly pillService:PillManageService){}
+    @ApiOperation({summary:'약 정보 조회'})
+    @ApiCreatedResponse({description:"성공",type:PillResponseDto})
+    @ApiForbiddenResponse({description:"실패",type:PillResponseErrorDto})
+    @ApiQuery({name:'name',required:true,description:'약 이름',example:'마그네슘'})
+	@Get()
+	async getPill(@Query('name') name:string, @Res() res:Response) {
+        const result = await this.pillService.getPill(name);
+		return res.json(result);
 	}
-    @Get('/:id')
-    getOnePill() {
-		return 'pill';
-	}
-    @Post()
-    addPill(){
-        
-    }
-    @Put('/:id')
-    putPill(){
 
-    }
-    @Delete('/:id')
-    deletePill(){
 
+
+
+    //Eat 기반으로 탐색
+    @ApiBearerAuth('access-token')
+    @ApiOperation({summary:'월별 섭취 정보'})
+    @ApiCreatedResponse({description:"성공",type:PillResponseErrorDto})//type 수정하기
+    @UseGuards(JwtAuthGuard)
+    @Get('month/')
+    async monthPill(@Query('month') month:number,@Res() res:Response){
+        return res.json();
+    }
+
+
+    //대폭 수정필요. userId 없어도 가능.
+    @ApiBearerAuth('access-token')
+    @ApiOperation({summary:'약 복용 여부'})
+    @ApiCreatedResponse({description:"성공",type:EatResponseDto})
+    @UseGuards(JwtAuthGuard)
+    @Post('/')
+    async takenPill(@Body(new ValidationPipe()) addPillDto:EatRequestDto,@Res() res:Response){
+        const userId=1;
+        const result = await this.pillService.takePill(addPillDto);
+        return res.json(result);
     }
 }
--- src/modules/pill_manage/pill_manage.module.ts
+++ src/modules/pill_manage/pill_manage.module.ts
@@ -1,10 +1,11 @@
 import { RepositoryModule } from '@modules/repo';
+import { UserManageModule } from '@modules/user_manage';
 import { Module } from '@nestjs/common';
 import { PillManageController } from './pill_manage.controller';
 import { PillManageService } from './pill_manage.service';
 
 @Module({
-    imports:[RepositoryModule],
+    imports:[RepositoryModule,UserManageModule],
     controllers: [PillManageController],
 	providers: [PillManageService],
 	exports: [PillManageService],
