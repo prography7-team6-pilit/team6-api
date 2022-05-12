5d1f9de46f47e423fa7242534ddff88db198bb59
Lee Jae Jun <linho301150@gmail.com>
2022-05-12 09:19:32 +0900

    Refactoring : Repository에서 작업하던 내용을 Service단으로 이동

--- src/modules/message_queue/message_queue.service.ts
+++ src/modules/message_queue/message_queue.service.ts
@@ -1,3 +1,4 @@
+import { Job } from '@modules/repo/entity/job.entity';
 import { RepositoryService } from '@modules/repo/repo.service';
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
@@ -39,16 +40,40 @@ export class MessageQueueService implements OnModuleDestroy{
         return result;
     }
 
-    async addMsg(jobDto:JobRequestPostDto):Promise<string>{ //작업 생성
+    async addMsg(userId:number,firebasetoken:string,jobDto:JobRequestPostDto):Promise<Job>{ //작업 생성
         const job=await this.msgq.add('transcode',{
             jobDto            
         },
         {
             repeat: { cron: "*/1 * * * *" }
         });
-        return job.id.toString();
+        const bullId = job.id.toString();
         //reapeat : cron
         //시간을 입력받으면 입력받은 값을 cron 형태로 변경하여 진행하기.
+        //실패시 오류 처리해놓기.
+        const jobEntity:Job={
+            alertId: 0,
+            alertTime: jobDto.alertTime,
+            isPush: jobDto.isPush,
+            userId: userId,
+            bullId: bullId,
+            Mon: false,Tue: false,Wed: false,Thu: false,Fri: false,Sat: false,Sun: false,eat:[],
+            firebasetoken:firebasetoken,
+        };
+        jobDto.alertWeek.forEach(element => {
+            switch (element){
+            case "Mon":jobEntity.Mon=true;break;
+            case "Tue":jobEntity.Tue=true;break;
+            case "Wed":jobEntity.Wed=true;break;
+            case "Thu":jobEntity.Thu=true;break;
+            case "Fri":jobEntity.Fri=true;break;
+            case "Sat":jobEntity.Sat=true;break;
+            case "Sun":jobEntity.Sun=true;break;
+            }        
+        });
+        const saveJob=await this.repo.repo_saveJob(jobEntity); //res.user 의 userId 가져오기
+        return saveJob;
+
     }
     async putMsg(){
         
--- src/modules/repo/repo.service.ts
+++ src/modules/repo/repo.service.ts
@@ -15,28 +15,7 @@ export class RepositoryService {
     @InjectRepository(Eat) private eatRepository:Repository<Eat>,
   ){}
 
-  async repo_saveJob(bullId:string,userId:number,jobDto: JobRequestPostDto): Promise<Job> {
-    //실패시 오류 처리해놓기.
-    const job:Job={
-      alertId: 0,
-      alertTime: jobDto.alertTime,
-      isPush: jobDto.isPush,
-      userId: userId,
-      bullId: bullId,
-      Mon: false,Tue: false,Wed: false,Thu: false,Fri: false,Sat: false,Sun: false,eat:[],
-
-    };
-    jobDto.alertWeek.forEach(element => {
-      switch (element){
-        case "Mon":job.Mon=true;break;
-        case "Tue":job.Tue=true;break;
-        case "Wed":job.Wed=true;break;
-        case "Thu":job.Thu=true;break;
-        case "Fri":job.Fri=true;break;
-        case "Sat":job.Sat=true;break;
-        case "Sun":job.Sun=true;break;
-      }        
-    });
+  async repo_saveJob(job:Job): Promise<Job> {
     const result=await this.jobRepository.save(job);
     return result;
   }
@@ -56,14 +35,8 @@ export class RepositoryService {
     const nickname=await this.userRepository.findOne({uuid:uuid});
     return nickname;
   }
-  async setNickname(userDto:UserRequestDto):Promise<User>{
-    const userEntity:User={
-      userId:0,
-      uuid:userDto.uuid,
-      nickname:userDto.nickname,
-      alert:[],
-      eat:[],
-    };
+
+  async setNickname(userEntity:User):Promise<User>{
     const result=await this.userRepository.save(userEntity);
     return result;
   }
