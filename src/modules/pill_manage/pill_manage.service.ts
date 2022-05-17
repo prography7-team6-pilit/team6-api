150aefb44743d69865f8f745fb2cf85d729683d4
Lee Jae Jun <linho301150@gmail.com>
2022-05-17 15:15:03 +0900

    Feat : 특정 일자의 요일 반환 코드 추가

--- src/modules/pill_manage/pill_manage.service.ts
+++ src/modules/pill_manage/pill_manage.service.ts
@@ -7,15 +7,24 @@ import { Eat } from '@modules/repo/entity/eat.entity';
 export class PillManageService {
     constructor(private readonly repo:RepositoryService){}
 
-    async takePill(userId:number,eatDto:EatRequestDto){
+    async getMonthPill(userId:number,year:number,month:number){
+        const week:Date=await this.strTodate(year,month,1);
+        const job=await this.repo.repo_getMonth(userId,week);
+        console.log(job);
+        return job;
+    }
+
+    async takePill(eatDto:EatRequestDto){
         const data:Eat={
             eatId:0,
-            userId:userId,
-            alertId:eatDto.alertId,
+            alertId:eatDto.eatId,
             eatDate:new Date(),
         }
         const result = await this.repo.repo_addPill(data);
-        return result;
+        //return result;
+
+        //Api dummy
+        return {result:true}
     }
 
     async putPill(){
@@ -24,5 +33,11 @@ export class PillManageService {
 
     async deletePill(){
         return 0;
+    }
+
+    async strTodate(year:number,month:number,day:number){
+        const strDate = year+'-'+month.toString().padStart(2,'0')+'-'+day.toString().padStart(2,'0');
+        const dateDate = new Date(strDate);
+        return dateDate;
     }
 }
