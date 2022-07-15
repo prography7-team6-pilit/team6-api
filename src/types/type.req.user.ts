6b3d73752691097af72803d392a30b41434c72bd
Lee Jae Jun <linho301150@gmail.com>
2022-07-15 13:37:36 +0900

    chore: 리펙토링으로 인한 변동사항

--- src/types/type.req.user.ts
+++ src/types/type.req.user.ts
@@ -1,7 +1,7 @@
-import { JwtDto } from '@modules/user_manage/dto/jwt.strategy.dto';
+import { JwtDto } from '@modules/user/dto/jwt.strategy.dto';
 
 declare global {
-  namespace Express {
-    export interface User extends JwtDto { }
-  }
+	namespace Express {
+		export interface User extends JwtDto {}
+	}
 }
